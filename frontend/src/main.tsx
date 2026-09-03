dbabd4ee69657eaf9762a4328815adc8df7d6186
codemonker1030 <codemonker1030@gmail.com>
2026-09-03 18:49:19 +0300

    Configure production API URL

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,5 +1,9 @@
 import { createRoot } from "react-dom/client";
+import { setBaseUrl } from "@workspace/api-client";
 import App from "./App";
 import "./index.css";
+
+const apiUrl = import.meta.env.VITE_API_URL;
+if (apiUrl) setBaseUrl(apiUrl);
 
 createRoot(document.getElementById("root")!).render(<App />);
